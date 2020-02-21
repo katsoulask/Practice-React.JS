f539f9f359fd4c0d8199fc77f017428b9607c4d9
Kkatsoulas <katsoulaskwstas@gmail.com>
2020-02-21 19:29:02 +0200

    splitting

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {Game} from "./game";
+import {Game} from "./Game";
 
 
 ReactDOM.render(<Game />, document.getElementById("root"));
